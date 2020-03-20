496c3cc26eb33b80315417fbcb900b960197a5d7
Sean Bailey <sepabailey@gmail.com>
2020-03-20 16:08:55 -0400

    Fixed line too long flake 8 error. Added authors

--- logpuzzle.py
+++ logpuzzle.py
@@ -12,11 +12,13 @@ http://code.google.com/edu/languages/google-python-class/
 Given an apache logfile, find the puzzle urls and download the images.
 
 Here's what a puzzle url looks like:
-10.254.254.28 - - [06/Aug/2007:00:13:48 -0700] "GET /~foo/puzzle-bar-aaab.jpg HTTP/1.0" 302 528 "-" "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"
+10.254.254.28 - - [06/Aug/2007:00:13:48 -0700] "GET /~foo/puzzle-bar-aaab.jpg
+ HTTP/1.0" 302 528 "-" "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US;
+  rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"
 
 """
 
-__author__ = "Sean"
+__author__ = "Sean Bailey, Koren Niles, Kano Marvel, Chelsea White, Ben Myers"
 
 import os
 import re
